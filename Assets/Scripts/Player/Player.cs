aac6697443ccbed23cad9d483268403ea9921958
Letovvvv <alex.tat521@gmail.com>
2026-05-24 20:35:08 +0300

    check player's grounded before jumping added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,23 +5,32 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
-    /// <summary>
-    /// sdh
-    /// </summary>
     [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private float canceledJumpSpeedMultiplier = 0.7f; //0 <= x < 1 какая часть скорости останется при отпускании кнопки прыжка
+    /// <summary>
+    /// какая часть скорости останется при отпускании кнопки прыжка
+    /// </summary>
+    [SerializeField] private float canceledJumpSpeedMultiplier = 0.7f; 
 
-    
-    private float moveX; //показания с A, D
-
+    /// <summary>
+    /// показания с A, D
+    /// </summary>
+    private float moveX;
+    /// <summary>
+    /// для IsGrounded
+    /// </summary>
+    private float checkGroundedDistance = 0.01f;
+    private Bounds colBounds;
+    private Vector2 rayOrigin;
 
     private Rigidbody2D rb;
+    private BoxCollider2D col;
     private InputSystem inp;
     
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
 
         inp = new InputSystem();
     }
@@ -61,7 +70,7 @@ public class Player : MonoBehaviour
 
     private void OnJump(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.started && IsGrounded())
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
@@ -71,4 +80,12 @@ public class Player : MonoBehaviour
         }
     }
 
+    private bool IsGrounded()
+    {
+        colBounds = col.bounds;
+        rayOrigin = new Vector2(colBounds.center.x, colBounds.min.y - 0.01f);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, checkGroundedDistance);
+
+        return hit.collider != null;
+    }
 }
